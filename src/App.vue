3a09ebf9d3448dc5844deb1cb4d2ff054a73398e
jason19901028 <jason_dlb@sina.cn>
2021-08-03 21:31:47 +0800

    add node

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
+<!--
+ * @Author: Aiden
+ * @Date: 2021-08-03 20:39:03
+ * @LastEditTime: 2021-08-03 21:29:09
+ * @LastEditors: Aiden
+ * @Description: 
+-->
 <template>
     <div class="wrapper">
-        app
+        <node :dataTree="dataTree"></node>
     </div>
 </template>
 <script>
-
+import datas from './data.json'
+import Node from '@/components/Node'
 export default {
-  name: "App"
+  name: "App",
+  props: {
+      dataTree: {
+          type: Array,
+          default: () => datas
+      }
+  },
+  components: {
+      Node,
+  },
+  mounted(){
+      console.log('data=========', datas)
+      console.log('dataTree===========', this.dataTree)
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 .wrapper{
-    background-color: #F00;
-    transform: scale(200);
-    display: flex;
 }
 </style>
---
+++ src/components/Node.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-for="(item) in dataTree" :key="item.id">
+      <span>
+        <span>title{{item.name}}</span>
+      </span>
+      <div v-if="item.children">
+        <node :dataTree="item.children"></node>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'node',
+  props: {
+    dataTree: {
+      type: Array,
+      default: function(){return []}
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
